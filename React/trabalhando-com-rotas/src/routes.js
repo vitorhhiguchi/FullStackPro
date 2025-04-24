8e3e624796c15692242d89ee95f0a3e67a833ecf
Vitor <vitordevestudos@gmail.com>
2025-04-23 23:42:19 -0300

    Evoluindo as Rotas

--- React/trabalhando-com-rotas/src/routes.js
+++ React/trabalhando-com-rotas/src/routes.js
@@ -4,9 +4,12 @@ import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 
+import Header from './components/Header';
+
 function RoutesApp(){ 
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> }/>
                 <Route path="/sobre" element={ <Sobre/> }/>
