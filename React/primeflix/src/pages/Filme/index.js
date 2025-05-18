4995d557b6ab4ea67789092e6ba06e606216e268
Vitor <vitordevestudos@gmail.com>
2025-05-18 14:54:47 -0300

    Página sobre de filmes

--- React/primeflix/src/pages/Filme/index.js
+++ React/primeflix/src/pages/Filme/index.js
@@ -1,8 +1,62 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './filme-info.css';
+
+import api from '../../services/api';
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "ed29abd0f9e9c5817d9487e6a99b2454",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=> {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log("Filme não encontrado");
+            })
+        }
+
+        loadFilme();
+
+
+        return () => {
+            console.log("Componente desmontado");
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes do filme...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem vindo a pagina detalhes do Filme</h1>
+        <div className="filme-info">
+            <h1>{ filme.title }</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+            
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
         </div>
     )
 }
