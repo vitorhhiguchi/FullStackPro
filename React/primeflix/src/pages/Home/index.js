ef0882ec4d680036dc8251ad437ad3d31481ec12
Vitor <vitordevestudos@gmail.com>
2025-05-18 01:49:51 -0300

    Criando Header, Home e Listagem de Filmes

---
+++ React/primeflix/src/pages/Home/index.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+// URL da API: https://api.themoviedb.org/3/movie/now_playing?api_key=ed29abd0f9e9c5817d9487e6a99b2454&language=pt-br
+
+function Home(){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "ed29abd0f9e9c5817d9487e6a99b2454",
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            // console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0,10))
+        }
+
+        loadFilmes();
+
+    }, [])
+
+    return(
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ React/primeflix/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// Base da URL: https://api.themoviedb.org/3/
+// URL DA API: movie/now_playing?api_key=ed29abd0f9e9c5817d9487e6a99b2454&language=pt-br
+
+const api =  axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
