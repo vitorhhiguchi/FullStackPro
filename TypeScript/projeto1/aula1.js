7f9312abee0d2efef00efe5485e5eae84df84c7b
Vitor <vitordevestudos@gmail.com>
2025-08-25 00:28:46 -0300

    Iniciando os estudos em Typescript

---
+++ TypeScript/projeto1/aula1.js
@@ -0,0 +1,2 @@
+var nome = "Vitor";
+console.log("Bem vindo " + nome);
---
+++ TypeScript/projeto1/aula1.ts
@@ -0,0 +1,5 @@
+// Quando compilamos o TS ele gera um JS.
+// O navegador não entende o TS, mas sim apenas o JS
+const nome = "Vitor"
+
+console.log("Bem vindo " + nome);
